5bc5431b47ddfed70277086fb5bf73882a6adfc3
eumerme <euthaisetau@gmail.com>
2022-06-30 12:02:45 -0300

    feat: botões criar quiz e voltar para home funcionando

--- scripts.js
+++ scripts.js
@@ -1,25 +1,83 @@
 const urlApi = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+let todosQuizzes;
+
+buscarTodosQuizzes ();
 
 
-
-
-function quizzSelecionado (quizzApi, elemento) {
+function toggleTela1 (){
     const containerTela1 = document.querySelector(".container-tela1");
-    containerTela1.classList.add("hide");
-
-    const containerTela2 = document.querySelector(".container-tela2");
-    containerTela2.classList.remove("hide");
-
-    renderizarUmQuizz(quizzApi);
+    containerTela1.classList.toggle("hide");
 }
 
-function renderizarUmQuizz (quizzApi) {
-    console.log(quizzApi)
+function toggleTela2 (){
+    const containerTela2 = document.querySelector(".container-tela2");
+    containerTela2.classList.toggle("hide");
+}
 
-    for (let i = 0; i < quizzApi.length; i++) {
+function toggleTela3 (){
+    const containerTela3 = document.querySelector(".forms-container");
+    containerTela3.classList.toggle("hide");
+}
+
+
+
+function buscarTodosQuizzes () {
+    const promise = axios.get(`${urlApi}`);
+    promise.catch(erro);
+    promise.then(todosquizzes);
+}
+
+function todosquizzes (quizzes) {
+    todosQuizzes = quizzes.data;
+    console.log(todosQuizzes);
+ 
+    renderizarTodosQuizzes();   
+}
+
+function erro (error) {
+    console.log(`erro`);
+}
+
+function renderizarTodosQuizzes () {
+    const quizzServer = document.querySelector(".box-todos-os-quizzes");
+
+    for (let i = 0; i < todosQuizzes.length; i++) {
+        const quizzTamplate = `
+            <li class="quizz-server" onclick="quizzSelecionado();">
+                <div class="titulo-quizz">${todosQuizzes[i].title}</div>
+                <img class="img-bckgnd" src="${todosQuizzes[i].image}" alt="">
+            </li>
+        `
+        quizzServer.innerHTML += quizzTamplate;
+    }
+
+    
+}
+
+function quizzSelecionado () {
+   toggleTela1();
+   toggleTela2 ();
+ 
+}
+
+// getOneQuizz();
+
+function getOneQuizz () {
+    const promise = axios.get(`${urlApi}/8000`);
+    promise.catch(erro);
+    promise.then(renderizarUmQuizz);
+}
+
+
+function renderizarUmQuizz (quizzApi) {
+    console.log(quizzApi.data)
+
+    /*const tela2 = document.querySelector(".container-tela2");
+    
+    for (let i = 0; i < quizzApi.data.length; i++) {
         const bannerTamplate = `
             <div class="banner">
-                <h1 class="titulo-banner">${quizzApi.title}</h1>
+                <h1 class="titulo-banner">${quizzApi.data.title}</h1>
             </div>
         `;
 
@@ -46,40 +104,22 @@ function renderizarUmQuizz (quizzApi) {
                 </div>
             </div>
         `;
-    }
+
+        tela2.innerHTML = bannerTamplate + 
+                        perguntaRespostaTamplate + 
+                        resultadoTamplate +
+                        `<div class="red-button">Reiniciar Quizz</div>
+
+                        <div class="voltar-home" onclick="quizzSelecionado();">Voltar pra home</div>
+                        `;
+    }*/
+
 }
 
-
-function renderizarTodosQuizzes (quizzes) {
-    console.log(quizzes.data);
-
-
-    const quizzServer = document.querySelector(".box-todos-os-quizzes");
-
-    for (let i = 0; i < quizzes.data.length; i++) {
-        const quizzTamplate = `
-            <li class="quizz-server" onclick="quizzSelecionado("${urlApi}/${quizzes.data[i].id}", this);">
-                <div class="titulo-quizz">${quizzes.data[i].title}</div>
-                <img class="img-bckgnd" src="${quizzes.data[i].image}" alt="">
-            </li>
-        `
-        quizzServer.innerHTML += quizzTamplate;
-    }
+function makeQuizz () {
+    toggleTela1();
+    toggleTela3();
 }
-
-function buscarTodosQuizzes () {
-    const promise = axios.get(`${urlApi}`);
-    promise.catch(erro);
-    promise.then(renderizarTodosQuizzes);
-}
-
-function erro (error) {
-    console.log(`erro`);
-}
-
-buscarTodosQuizzes ();
-
-
 
 const container = document.querySelector('.container');
 function basicInformationsQuizz(element) {
